11d0dcb256389fe2cfea06246cf0cf1ff49336e3
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-12-17 13:59:43 +0200

    skiscript

---
+++ Assets/SkiScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SkiScript : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			rb.isKinematic = false;
+		}
+		if (Input.GetKeyDown(KeyCode.Mouse1)) {
+			SceneManager.LoadScene("scene");
+		}
+	}
+}
